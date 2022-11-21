fd2308b7aed5d191e74b7b7273b8035629991585
LuisAlejandroRamos <104103476+LuisAlejandroRamos@users.noreply.github.com>
2022-11-20 18:08:08 -0600

    Add files via upload

---
+++ REST/config.php
@@ -0,0 +1,10 @@
+ <?php
+
+$db = [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'db' => 'rest' //Cambiar al nombre de tu base de datos
+];
+
+?>
---
+++ REST/index.php
@@ -0,0 +1,8 @@
+<?php
+
+header("HTTP/1.1 200")
+
+IF ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	
+}
+?>
